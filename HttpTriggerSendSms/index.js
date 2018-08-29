d78a87f5ccc049f64cf9b95f1b8becf66262bb74
Neo Ho <neofelisho@gmail.com>
2018-08-29 17:44:59 +0800

    Add sending sms API.

--- HttpTriggerSendSms/index.js
+++ HttpTriggerSendSms/index.js
@@ -1,33 +1,25 @@
+/**
+ * API: Send SMS message
+ * Method: Http POST
+ */
+
 'use strict'
 
 module.exports = function (context, req) {
   context.log('HTTP trigger function SMS service processed a request.')
 
-  let msg = ''
-  if (req.body) {
-    let contact = req.body.contact
-    let message = req.body.message
-    if (contact && message) {
-      try {
-      // put data into storage queue
-
-      } catch (err) {
-
-      }
-      context.res = {
-        status: 200,
-        body: `Success, send ${message} to ${contact}.`
-      }
-      context.done()
+  if (req.body && req.body.contact && req.body.message) {
+    // ToDo: Check paramters format here
+    context.bindings.queueMessage = JSON.stringify(req.body)
+    context.res = {
+      status: 200,
+      body: `Success, send ${req.body.message} to ${req.body.contact}.`
     }
-    if (!contact) msg += 'Contact number is missing.'
-    if (!message) msg += 'Message content is missing.'
   } else {
-    msg = 'Please pass data in the request body.'
-  }
-  context.res = {
-    status: 400,
-    body: msg
+    context.res = {
+      status: 400,
+      body: 'Please pass data in the request body.'
+    }
   }
   context.done()
 }
