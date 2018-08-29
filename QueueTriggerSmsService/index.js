23d11ffa46d654e8f6095a766d35de7fcb8f9555
Neo Ho <neofelisho@gmail.com>
2018-08-29 18:42:59 +0800

    Add queue trigger function.

--- QueueTriggerSmsService/index.js
+++ QueueTriggerSmsService/index.js
@@ -1,4 +1,13 @@
-module.exports = function (context, smsMessage) {
-    context.log('JavaScript queue trigger function processed work item', smsMessage);
-    context.done();
-};
+'use strict'
+
+module.exports = function (context, queueMessage) {
+  let contact = queueMessage.contact
+  let message = queueMessage.message
+
+  // check latest log
+  // case(no log): use default sms service
+  // case(re-try): use another sms service
+  // case(success): use same sms service
+  context.log('JavaScript queue trigger function processed work item', contact + ':' + message)
+  context.done()
+}
