642b560801f6426aad8090b72cfdcef95668f827
Neo Ho <neofelisho@gmail.com>
2018-08-30 18:21:31 +0800

    Add storage table binding to SendSms API.

--- HttpTriggerSendSms/index.js
+++ HttpTriggerSendSms/index.js
@@ -5,12 +5,15 @@
 
 'use strict'
 
+const logFormater = require('../lib/log_formater')
+
 module.exports = function (context, req) {
   context.log('HTTP trigger function SMS service processed a request.')
 
-  if (req.body && req.body.contact && req.body.message) {
+  if (req.body && req.body.platform && req.body.username && req.body.contact && req.body.message) {
     // ToDo: Check paramters format here
-    context.bindings.queueMessage = JSON.stringify(req.body)
+    context.bindings.tableBinding = logFormater(req.body.platform, req.body.username, req.body.contact, req.body.message, '', 0)
+    context.bindings.queueMessage = req.body.contact
     context.res = {
       status: 200,
       body: `Success, send ${req.body.message} to ${req.body.contact}.`
